e138a43a5aaa63ce014f410c4e567c9ecdfbd349
Tomas <tomasdimauro1@gmail.com>
2020-12-03 18:49:39 -0300

    Second commit

---
+++ head.php
@@ -0,0 +1,12 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lbry-Job</title>
+    <meta name="robots" content="noindex" />
+    <meta name="googlebot" content="noindex" />
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" rel="stylesheet">
+</head>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include('head.php');
+?>
+<body style="font-family: 'Overpass', sans-serif;">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+</body>
